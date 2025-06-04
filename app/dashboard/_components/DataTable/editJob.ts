b5fb8e2bb624e6a76aeca5d1ebf9fa246e0b5686
Ben <bennylopezhernandez0@gmail.com>
2025-06-03 22:04:56 -0400

    Close: #4

--- app/dashboard/_components/DataTable/editJob.ts
+++ app/dashboard/_components/DataTable/editJob.ts
@@ -3,19 +3,25 @@ import { JobApp } from "@/lib/data/models";
 import { editSchema } from "@/lib/zodSchemas/editSchema";
 import { db } from "@/server/db/db";
 import { jobs } from "@/server/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
 export async function editJob(formData: JobApp) {
+  const { userId } = await auth();
   const parse = editSchema.safeParse({ ...formData });
 
-  try {
-    await db.update(jobs).set({
-      ...parse.data,
-      updatedAt: new Date(Date.now()),
-      createdAt: jobs.createdAt,
-    });
-  } catch (e) {
-    console.error(e);
+  if (userId) {
+    try {
+      await db.update(jobs).set({
+        ...parse.data,
+        userId: userId,
+        updatedAt: new Date(Date.now()),
+        createdAt: jobs.createdAt,
+      }).where(eq(jobs.id, parse.data?.id!));
+    } catch (e) {
+      console.error(e);
+    }
   }
   revalidatePath("/dashboard");
 }
