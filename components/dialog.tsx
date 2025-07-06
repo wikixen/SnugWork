0a2bbf3abf31edb3fff6bf58ca5add622650cdc0
Ben <bennylopezhernandez0@gmail.com>
2025-07-06 19:05:11 -0400

    removed unused imports

--- components/dialog.tsx
+++ components/dialog.tsx
@@ -1,5 +1,5 @@
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { Dispatch, ReactNode, SetStateAction, useState } from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Dispatch, ReactNode, SetStateAction } from 'react';
 
 interface Props {
   trigger: ReactNode;
